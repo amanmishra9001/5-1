a7157deddfcd879749f186750b024bf80b4b4382
amanmishra9001 <116724681+amanmishra9001@users.noreply.github.com>
2022-11-13 08:36:30 +0530

    Add files via upload

---
+++ Assignment 5/pr_01.cpp
@@ -0,0 +1,14 @@
+// Assignemnt number 5 is nothing but a generalised formof program of the Assignent number 4
+
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    printf("enter a number ");
+    scanf("%d",&a);
+    for (i=0 ; i<a ; i++)
+    {
+        printf("\nAman");
+    }
+    printf("\n");
+}
